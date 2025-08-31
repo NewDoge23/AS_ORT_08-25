d38564c6ce3441d130a1e4948e8da8caebf85006
BrunoRamadori <Bruno.Ramadori.43@gmail.com>
2025-08-31 02:03:18 -0300

    Realizo Ejercicios 19 y 20.

--- BrunoRamadori/THP/TrabajoPractico1/src/actividad2/Ejercicio19.java
+++ BrunoRamadori/THP/TrabajoPractico1/src/actividad2/Ejercicio19.java
@@ -1,4 +1,31 @@
 package actividad2;
 
+import java.util.Scanner;
+
 public class Ejercicio19 {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        boolean estaEnAmbosGrupos, noEstaEnNingunGrupo, esImpar, esDeUnSoloDigito;
+        int num;
+
+        System.out.println("Por favor, ingrese un número entero:");
+        num = Integer.parseInt(input.nextLine());
+
+        esImpar = (num % 2) == 1;
+        esDeUnSoloDigito = (num >= -9 && num <= 9);
+        estaEnAmbosGrupos = esImpar && esDeUnSoloDigito;
+        noEstaEnNingunGrupo = !(esImpar || esDeUnSoloDigito);
+
+        System.out.println("El número ingresado es: " + num);
+        System.out.println("Es Impar: " + esImpar);
+        System.out.println("Es de un solo dígito: " + esDeUnSoloDigito);
+        System.out.println("Está en ambos grupos: " + estaEnAmbosGrupos);
+        System.out.println("No está en ningún grupo: " + noEstaEnNingunGrupo);
+
+        input.close();
+
+    }
 }
--- BrunoRamadori/THP/TrabajoPractico1/src/actividad2/Ejercicio20.java
+++ BrunoRamadori/THP/TrabajoPractico1/src/actividad2/Ejercicio20.java
@@ -1,4 +1,60 @@
 package actividad2;
 
+import java.util.Scanner;
+
 public class Ejercicio20 {
+
+    static Scanner input = new Scanner(System.in);
+
+    final static char SUMA = '+';
+    final static char RESTA = '-';
+    final static char MULTI = '*';
+    final static char DIV = '/';
+
+    public static void main(String[] args) {
+
+        int num1, num2, resultado;
+        char op;
+
+        System.out.println("Por favor, ingrese el primer número entero");
+        num1 = Integer.parseInt(input.nextLine());
+
+        System.out.println("Por favor, ingrese el segundo número entero");
+        num2 = Integer.parseInt(input.nextLine());
+
+        System.out.println("Por favor, ingrese la operación a realizar");
+        op = input.nextLine().strip().charAt(0);
+
+        switch (op) {
+            case SUMA: {
+                resultado = num1 + num2;
+                System.out.println("EL resultado de la suma es " + resultado);
+                break;
+            }
+            case RESTA: {
+                resultado = num1 - num2;
+                System.out.println("EL resultado de la resta es " + resultado);
+                break;
+            }
+            case MULTI: {
+                resultado = num1 * num2;
+                System.out.println("EL resultado de la multiplicación es " + resultado);
+                break;
+            }
+            case DIV: {
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("EL resultado de la división es " + resultado);
+                } else {
+                    System.out.println("ERROR --------- NO se puede dividir por cero mogólico de mierda");
+                }
+                break;
+            }
+            default: {
+                System.out.println("Operación invalida");
+            }
+        }
+
+        input.close();
+    }
 }
